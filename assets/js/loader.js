fcad3c55ad19491b7ad0816540f83f271ec1bfef
Sirinya Richardson <sirinya@lemonbrain.ch>
2023-01-13 19:40:11 +0100

    Fix jshint errors in loader.js file.

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,32 +1,41 @@
+/*jshint esversion: 6 */
+
+/*jslint       browser: true, continue: true,
+ devel: true, indent: 2, maxerr: 50,
+ newcap: true, nomen: true, plusplus: true,
+ regexp: true, sloppy: true, vars: false,
+ white: true
+ */
+
 (function () {
     const CLASS = {
         LOADER: '.loader'
-    }
+    };
 
     const EVENT = {
         LOAD: 'load'
-    }
+    };
 
     const htmlElementMap = {
         loader: ''
-    }
+    };
 
     const initHTMLElements = () => {
         htmlElementMap.loader = document.querySelector(CLASS.LOADER);
-    }
+    };
 
     const initEvents = () => {
         window.addEventListener(EVENT.LOAD, () => {
-            pageElements = document.querySelectorAll('body > *:not(div.loader):not(script)');
+            let pageElements = document.querySelectorAll('body > *:not(div.loader):not(script)');
 
             pageElements.forEach((el) => {
                 el.style.visibility = 'visible';
-            })
+            });
 
             htmlElementMap.loader.style.display = 'none';
-        })
-    }
+        });
+    };
 
-    initHTMLElements()
-    initEvents()
+    initHTMLElements();
+    initEvents();
 })();
