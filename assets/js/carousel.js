b9c70be112c7582347e03be0ac3e3826d431d84d
Sirinya Richardson <sirinya@lemonbrain.ch>
2023-01-13 19:38:08 +0100

    Remove private class fields from carousel.js javascript file, as does not pass jshint test.

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -7,69 +7,48 @@
  */
 
 /* https://stackoverflow.com/questions/15338054/horizontal-slideshow-with-divs */
-/* JS Hint error but is relevant https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Private_class_fields */
-
-/* https://github.com/jshint/jshint/issues/3361 */
 class Carousel {
-    #htmlElements = {
-        carouselContainer: '',
-        carouselArea: '',
-        carouselPages: '',
-        previousBtn: '',
-        nextBtn: ''
-    };
-    #settings = {
-        ids: {
-            carousel: ''
-        },
-        classes: {
-            carouselArea: '',
-            carouselPage: '',
-            previousBtn: '',
-            nextBtn: ''
-        },
-        events: {
-            click: 'click'
-        },
-        carouselPageNumber: 0,
-        counter: 0,
-        intervalTime: null
-    };
-
     constructor(carousel, carouselArea, carouselPage, previousBtn, nextBtn) {
-        this.#settings.ids.carousel = carousel;
-        this.#settings.classes.carouselArea = carouselArea;
-        this.#settings.classes.carouselPage = carouselPage;
-        this.#settings.classes.previousBtn = previousBtn;
-        this.#settings.classes.nextBtn = nextBtn;
+        this.carouselId = carousel;
+        this.carouselAreaClass = carouselArea;
+        this.carouselPageClass = carouselPage;
+        this.previousBtnClass = previousBtn;
+        this.nextBtnClass = nextBtn;
+        this.carouselPageNumber = 0;
+        this.counter = 0;
+        this.carouselContainer = '';
+        this.carouselArea = '';
+        this.carouselPages = '';
+        this.previousBtn = '';
+        this.nextBtn = '';
 
-        if (document.querySelector(this.#settings.ids.carousel)) {
-            this.#htmlElements.carouselContainer = document.querySelector(this.#settings.ids.carousel);
+        if (document.querySelector(this.carouselId)) {
+            this.carouselContainer = document.querySelector(this.carouselId);
             this.initHTMLElements();
             this.initEvents();
         }
     }
 
     initHTMLElements() {
-        this.#htmlElements.carouselArea = this.#htmlElements.carouselContainer.querySelector(`.${this.#settings.classes.carouselArea}`);
-        this.#htmlElements.carouselPages = this.#htmlElements.carouselContainer.querySelectorAll(`.${this.#settings.classes.carouselPage}`);
-        this.#htmlElements.previousBtn = this.#htmlElements.carouselContainer.querySelector(`.${this.#settings.classes.previousBtn}`);
-        this.#htmlElements.nextBtn = this.#htmlElements.carouselContainer.querySelector(`.${this.#settings.classes.nextBtn}`);
+        this.carouselArea = this.carouselContainer.querySelector(`.${this.carouselAreaClass}`);
+        this.carouselPages = this.carouselContainer.querySelectorAll(`.${this.carouselPageClass}`);
+        this.previousBtn = this.carouselContainer.querySelector(`.${this.previousBtnClass}`);
+        this.nextBtn = this.carouselContainer.querySelector(`.${this.nextBtnClass}`);
 
-        if (this.#htmlElements.carouselPages) {
-            this.#settings.carouselPageNumber = this.#htmlElements.carouselPages.length;
+        if (this.carouselPages) {
+            this.carouselPageNumber = this.carouselPages.length;
         }
     }
 
     initEvents() {
-        this.#htmlElements.previousBtn.addEventListener(this.#settings.events.click, e => this.slide(e));
-        this.#htmlElements.nextBtn.addEventListener(this.#settings.events.click, e => this.slide(e));
+        this.previousBtn.addEventListener('click', e => this.slide(e));
+        this.nextBtn.addEventListener('click', e => this.slide(e));
     }
 
     slide(e) {
-        const currentCounter = e.currentTarget.classList.contains(this.#settings.classes.previousBtn) ? --this.#settings.counter : ++this.#settings.counter;
-        this.#settings.counter = currentCounter < 0 ? parseInt(this.#settings.carouselPageNumber) - 1 : parseInt(this.#settings.counter) % parseInt(this.#settings.carouselPageNumber);
-        this.#htmlElements.carouselArea.style.transform = `translateX(-${parseInt(this.#settings.counter) * 100}%)`;
+        const currentCounter = e.currentTarget.classList.contains(this.previousBtnClass) ? --this.counter : ++this.counter;
+        this.counter = currentCounter < 0 ? parseInt(this.carouselPageNumber) - 1 : parseInt(this.counter) % parseInt(this.carouselPageNumber);
+        this.carouselArea.style.transform = `translateX(-${parseInt(this.counter) * 100}%)`;
     }
 }
 
