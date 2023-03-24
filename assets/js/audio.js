b5d3a74b3b7bc1c0793ebd87f4a5a2bfa1d0e889
Sirinya Richardson <siri.richardson@gmail.com>
2023-03-24 14:03:16 +0100

    Add async and await to audio play()

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -1,13 +1,13 @@
 class Audio {
-    constructor(
-        buttonContainer,
-        musicButtonAudioClass,
-        silentClass
-    ) {
-        this.buttonContainerSelector = buttonContainer;
-        this.musicButtonAudioSelector = musicButtonAudioClass;
-        this.silentSelector = silentClass;
-    }
+  constructor(
+    buttonContainer,
+    musicButtonAudioClass,
+    silentClass
+  ) {
+    this.buttonContainerSelector = buttonContainer;
+    this.musicButtonAudioSelector = musicButtonAudioClass;
+    this.silentSelector = silentClass;
+  }
 }
 
 const audio = new Audio('#music-button', 'button__music--audio', 'silent');
@@ -17,18 +17,18 @@ const audioElement = buttonContainer.querySelector(`.${audio.musicButtonAudioSel
 const slashIcon = buttonContainer.querySelector(`.${audio.silentSelector}`);
 
 
-function play() {
-    if (audioElement.paused) {
-        audioElement.play();
-    } else {
-        audioElement.pause();
-    }
+async function play() {
+  if (audioElement.paused) {
+    await audioElement.play();
+  } else {
+    audioElement.pause();
+  }
 
-    if (slashIcon.classList.contains('active')) {
-        slashIcon.classList.remove('active');
-    } else {
-        slashIcon.classList.add('active');
-    }
+  if (slashIcon.classList.contains('active')) {
+    slashIcon.classList.remove('active');
+  } else {
+    slashIcon.classList.add('active');
+  }
 }
 
 buttonContainer.addEventListener('click', play);
