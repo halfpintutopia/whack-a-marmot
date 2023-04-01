fa8b1eec36de4b2b42cc6fe56ceb21a85e7b5cfd
Sirinya Richardson <siri.richardson@gmail.com>
2023-04-01 13:47:11 +0200

    Add JS Doc strings to the Modal class file.

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,4 +1,13 @@
+/**
+ * Creates a modal if a button and modal has been created in the HTML
+ */
 class Modal {
+  /**
+   * Initiates the modal when button, modal and overlay are available
+   * @param {string} buttonId
+   * @param {string} modalId
+   * @param {string} overlayId
+   */
   constructor(buttonId, modalId, overlayId) {
     this.buttonId = buttonId;
     this.modalId = modalId;
@@ -7,11 +16,11 @@ class Modal {
     this.hiddenClass = 'hidden';
 
     if (this.button) {
-      this.button.addEventListener('click', this.openModalHandler.bind(this));
+      this.button.addEventListener('click', this.openModal.bind(this));
     }
     if (this.modal && this.modal.querySelector(this.modalCloseButtonClass)) {
       const btnClose = this.modal.querySelector(this.modalCloseButtonClass);
-      btnClose.addEventListener('click', this.closeModalHandler.bind(this));
+      btnClose.addEventListener('click', this.closeModal.bind(this));
     }
   }
 
@@ -27,17 +36,33 @@ class Modal {
     return document.getElementById(this.modalId);
   }
 
-  openModalHandler() {
+  /**
+   * Opens the modal when the button is clicked
+   */
+  openModal() {
     this.modal.classList.remove(this.hiddenClass);
+    this.showModalOverlay();
+  }
+
+  /**
+   * Closes the button when the close button is clicked
+   */
+  closeModal() {
+    this.modal.classList.add(this.hiddenClass);
+    this.hideModalOverlay();
+  }
+
+  /**
+   * The modal overlay is visible when the modal has been opened
+   */
+  showModalOverlay() {
     this.modalOverlay.classList.remove(this.hiddenClass);
   }
 
-  closeModalHandler() {
-    this.modal.classList.add(this.hiddenClass);
-    this.closeModalOverlay();
-  }
-
-  closeModalOverlay() {
+  /**
+   * The overlay is hidden when the modal has been closed
+   */
+  hideModalOverlay() {
     this.modalOverlay.classList.add(this.hiddenClass);
   }
 }
