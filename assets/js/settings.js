1275c4b95ac185c5a3072743bbaa840b4684db97
Sirinya Richardson <siri.richardson@gmail.com>
2023-04-02 09:59:41 +0200

    Add JS docs strings for the Settings class. Add setDisplay function to set the display to dark or light when chosen.

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,4 +1,11 @@
+/**
+ * Sets the settings chosen by the user in the localstorage
+ */
 class Settings {
+  /**
+   * Initiates the settings
+   * @param buttonContainerClass
+   */
   constructor(buttonContainerClass) {
     this.buttonContainerClass = buttonContainerClass;
     this.display = 'display';
@@ -10,21 +17,34 @@ class Settings {
     if (this.settingOptions) {
       this.setSettings(this.display);
       this.setSettings(this.difficulty);
+      this.setDisplay();
 
       this.settingOptions.forEach(settingOption => {
-        settingOption.addEventListener('change', this.switchAndSetLocalStorage);
+        settingOption.addEventListener('change', this.switchAndSetLocalStorage.bind(this));
       });
     }
   }
 
+  /**
+   * Gets the settings container element
+   * @returns {*}
+   */
   get settingsContainer() {
     return document.querySelector(this.buttonContainerClass);
   }
 
+  /**
+   * Gets the setting options from the modal container
+   * @returns {NodeListOf<Element> | NodeListOf<SVGElementTagNameMap[keyof SVGElementTagNameMap]> | NodeListOf<HTMLElementTagNameMap[keyof HTMLElementTagNameMap]>}
+   */
   get settingOptions() {
     return this.settingsContainer.querySelectorAll('input[type="checkbox"].toggle');
   }
 
+  /**
+   * Gets the display settings from the local storage
+   * @returns {string}
+   */
   get displaySetting() {
     if (localStorage.getItem(this.display) === '') {
       localStorage.setItem(this.display, this.displayDefault);
@@ -32,6 +52,10 @@ class Settings {
     return localStorage.getItem(this.display);
   }
 
+  /**
+   * Gets the difficulty settings from the local storage
+   * @returns {string}
+   */
   get difficultySetting() {
     if (localStorage.getItem(this.difficulty) === '') {
       localStorage.setItem(this.difficulty, this.difficultyDefault);
@@ -39,6 +63,10 @@ class Settings {
     return localStorage.getItem(this.difficulty);
   }
 
+  /**
+   * Sets the settings and stores them in local storage
+   * @param settingType
+   */
   setSettings(settingType) {
     const input = document.querySelector(`[data-input="${settingType}"]`);
     const labelSpans = input.nextElementSibling.querySelectorAll(`[data-${settingType}]`);
@@ -55,6 +83,10 @@ class Settings {
     });
   }
 
+  /**
+   * Changes the settings in the local storage
+   * @param switchOptionEvent
+   */
   switchAndSetLocalStorage(switchOptionEvent) {
     let choice;
     const spans = switchOptionEvent.target.nextElementSibling.querySelectorAll('span');
@@ -70,6 +102,20 @@ class Settings {
       spans[1].classList.add('off');
       choice = spans[0].getAttribute(`data-${switchOptionEvent.target.dataset.input}`);
       localStorage.setItem(switchOptionEvent.target.dataset.input, choice);
+    }
+
+    this.setDisplay();
+  }
+
+  /**
+   * Switches between dark and light mode
+   */
+  setDisplay() {
+    const displaySetting = localStorage.getItem('display');
+    if (displaySetting === 'dark') {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
     }
   }
 }
