42d930c7d9b95c754d9fb482b0bcd168d3a1681c
Sirinya Richardson <siri.richardson@gmail.com>
2023-04-02 09:58:37 +0200

    Add JS docs strings for the Board class

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,14 +1,33 @@
-import {createGameButtons} from "./gameButton.js";
-import {debounce} from "./helpers.js";
-
-export class Board {
+/**
+ * Board class for creating, updating, changing the board according to view size
+ */
+class Board {
+  /**
+   * Creates, updates board
+   * @param {string} gameId
+   */
   constructor(gameId) {
+    // IDs
     this.gameId = gameId;
     this.exitBtnId = 'exit-btn';
-    this.screenSize = '';
-    this.gameResponsiveMinWidth = 768;
-    this.desktopClass = 'desktop';
-    this.mobileClass = 'mobile';
+
+    // Classes
+    this.activeCls = 'active';
+    this.desktopCls = 'desktop';
+    this.mobileCls = 'mobile';
+    this.holesContainerCls = '.holes-container';
+    this.holeCls = 'hole';
+    this.marmotContainerCls = 'marmot__container';
+    this.holeContainerCls = 'hole__container';
+    this.marmotImageCls = 'marmot__img';
+
+    // Image attributes
+    this.marmotImageSrc = 'assets/media/images/marmot.svg';
+    this.marmotImageAlt = 'Marmot';
+
+    // Settings:
+    this.numHolesForDesktopView = 12;
+    this.numHolesForMobileView = 6;
     this.gameGridColumn = 0;
     this.gameGridRow = 0;
     this.gameGridColumnMobile = 3;
@@ -16,93 +35,93 @@ export class Board {
     this.gameGridRowMobile = 2;
     this.gameGridRowDesktop = 4;
     this.numberOfHoles = 0;
-    this.holeClass = 'hole';
-    this.marmotContainerClass = 'marmot__container';
-    this.holeContainerClass = 'hole__container';
-    this.marmotImageClass = 'marmot__img';
-    this.marmotImageSrc = 'assets/media/images/marmot.svg';
-    this.marmotImageAlt = 'Marmot';
+    this.screenSize = '';
+    this.gameResponsiveMinWidth = 768;
+    this.resetToEmpty = '';
   }
 
+  /**
+   * Gets the game element
+   * @returns {HTMLElement}
+   */
+  get game() {
+    return document.getElementById(this.gameId);
+  }
+
+  /**
+   * Gets the holes container element
+   * @returns {Element}
+   */
+  get holesContainer() {
+    return document.querySelector(this.holesContainerCls);
+  }
+
+
+  /**
+   * Reset the game board.
+   * Removes all the holes from the board.
+   */
   removeGridLayout() {
+    this.holesContainer.innerHTML = this.resetToEmpty;
+  }
+
+  /**
+   * Changes the board size and the number of holes to display,
+   * dependent on view port.
+   */
+  changeGridLayout() {
+    this.holesContainer.innerHTML = this.resetToEmpty;
+
+    if (window.innerWidth > this.gameResponsiveMinWidth) {
+      document.body.classList.add(this.desktopCls);
+      document.body.classList.remove(this.mobileCls);
+      this.screenSize = this.desktopCls;
+      this.numberOfHoles = this.numHolesForDesktopView;
+      this.gameGridColumn = this.gameGridColumnDesktop;
+      this.gameGridRow = this.gameGridRowDesktop;
+    } else {
+      document.body.classList.add(this.mobileCls);
+      document.body.classList.remove(this.desktopCls);
+      this.screenSize = this.mobileCls;
+      this.numberOfHoles = this.numHolesForMobileView;
+      this.gameGridColumn = this.gameGridColumnMobile;
+      this.gameGridRow = this.gameGridRowMobile;
+    }
+  }
+
+  /**
+   * Creates the marmot holes for the game board
+   */
+  createMarmotHoles() {
+    const start = this.screenSize === 'desktop' ? 2 : 1;
     const holesContainer = document.querySelector('.holes-container');
-    holesContainer.innerHTML = '';
-  }
-}
 
-const board = new Board('game-area', '.game-buttons');
-const game = document.getElementById(board.gameId);
-const holesContainer = document.querySelector('.holes-container');
-const exitButton = document.getElementById(board.exitBtnId);
+    for (let i = start; i <= this.gameGridRow; i++) {
+      for (let j = 1; j <= this.gameGridColumn; j++) {
+        const hole = document.createElement('div');
+        hole.classList.add(this.holeCls);
 
-export function changeGridLayout() {
-  holesContainer.innerHTML = '';
+        hole.style.gridArea = `${i} / ${j} / ${i + 1} / ${j + 1}`;
 
-  if (window.innerWidth > board.gameResponsiveMinWidth) {
-    document.body.classList.add(board.desktopClass);
-    document.body.classList.remove(board.mobileClass);
-    board.screenSize = 'desktop';
-    board.numberOfHoles = 12;
-    board.gameGridColumn = board.gameGridColumnDesktop;
-    board.gameGridRow = board.gameGridRowDesktop;
-  } else {
-    document.body.classList.add(board.mobileClass);
-    document.body.classList.remove(board.desktopClass);
-    board.screenSize = 'mobile';
-    board.numberOfHoles = 6;
-    board.gameGridColumn = board.gameGridColumnMobile;
-    board.gameGridRow = board.gameGridRowMobile;
-  }
-}
+        const marmotContainer = document.createElement('div');
+        marmotContainer.classList.add(this.marmotContainerCls);
 
-export function initExitGame() {
-  game.classList.remove('active');
-  board.removeGridLayout();
-  showHideExitBtn();
-  createGameButtons();
-}
+        const holeContainer = document.createElement('div');
+        holeContainer.classList.add(this.holeContainerCls);
+        let randomDirtImageNumber = Math.floor((Math.random() * 5 - 1) + 1) + 1;
+        holeContainer.style.backgroundImage = `url('assets/media/images/dirt-mounds/dirt-${randomDirtImageNumber}.svg')`;
 
-exitButton.addEventListener('click', initExitGame);
+        const marmotImage = document.createElement('img');
+        marmotImage.classList.add(this.marmotImageCls);
+        marmotImage.setAttribute('draggable', 'false');
+        marmotImage.setAttribute('src', this.marmotImageSrc);
+        marmotImage.setAttribute('alt', this.marmotImageAlt);
 
-export function showHideExitBtn() {
-  if (exitButton.classList.contains('hidden')) {
-    exitButton.classList.remove('hidden');
-    exitButton.addEventListener('click', initExitGame);
-  } else {
-    exitButton.removeEventListener('click', initExitGame);
-    exitButton.classList.add('hidden');
-  }
-}
+        marmotContainer.append(holeContainer, marmotImage);
 
-window.addEventListener('resize', debounce(changeGridLayout, 500));
-
-export function createMarmotHoles() {
-  const start = board.screenSize === 'desktop' ? 2 : 1;
-  const holesContainer = document.querySelector('.holes-container');
-
-  for (let i = start; i <= board.gameGridRow; i++) {
-    for (let j = 1; j <= board.gameGridColumn; j++) {
-      const hole = document.createElement('div');
-      hole.classList.add(board.holeClass);
-
-      hole.style.gridArea = `${i} / ${j} / ${i + 1} / ${j + 1}`;
-
-      const marmotContainer = document.createElement('div');
-      marmotContainer.classList.add(board.marmotContainerClass);
-
-      const holeContainer = document.createElement('div');
-      holeContainer.classList.add(board.holeContainerClass);
-
-      const marmotImage = document.createElement('img');
-      marmotImage.classList.add(board.marmotImageClass);
-      marmotImage.setAttribute('draggable', 'false');
-      marmotImage.setAttribute('src', board.marmotImageSrc);
-      marmotImage.setAttribute('alt', board.marmotImageAlt);
-
-      marmotContainer.append(holeContainer, marmotImage);
-
-      hole.append(marmotContainer);
-      holesContainer.append(hole);
+        hole.append(marmotContainer);
+        holesContainer.append(hole);
+      }
     }
   }
 }
